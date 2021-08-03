6f48a14a15005a72c197549087cefe1368db1d90
Yacine Rezgui <yrezgui@google.com>
2021-08-03 11:50:27 +0200

    Add AppInstaller initial files

---
+++ AppInstaller/app/src/main/java/com/samples/appinstaller/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.samples.appinstaller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.samples.appinstaller.ui.theme.AppInstallerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppInstallerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppInstallerTheme {
+        Greeting("Android")
+    }
+}
