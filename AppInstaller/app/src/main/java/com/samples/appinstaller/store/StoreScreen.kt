97f380ffe28bfea21cea6fa2638fdeaa98b77e13
Yacine Rezgui <yrezgui@google.com>
2021-10-04 10:34:24 +0900

    Apply spotless

--- AppInstaller/app/src/main/java/com/samples/appinstaller/store/StoreScreen.kt
+++ AppInstaller/app/src/main/java/com/samples/appinstaller/store/StoreScreen.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.samples.appinstaller.store
 
 import android.text.format.DateUtils
