d80f24bb7491db02d13e42819166a1f51c6028c5
Yacine Rezgui <yrezgui@google.com>
2021-10-15 14:38:53 +0100

    Upgrade AGP to 7.0.3

--- AppInstaller/build.gradle.kts
+++ AppInstaller/build.gradle.kts
@@ -21,7 +21,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("com.android.tools.build:gradle:7.0.3")
         classpath(kotlin("gradle-plugin", version = "1.5.21"))
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
 
