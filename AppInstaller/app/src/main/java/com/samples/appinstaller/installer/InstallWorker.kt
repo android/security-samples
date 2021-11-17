cd1498be31f99e1c3d5af4f62903db1a96b57a5b
Yacine Rezgui <rezgui.y@gmail.com>
2021-11-17 17:17:48 +0000

    Lower downloading delay for dev purpose

--- AppInstaller/app/src/main/java/com/samples/appinstaller/installer/InstallWorker.kt
+++ AppInstaller/app/src/main/java/com/samples/appinstaller/installer/InstallWorker.kt
@@ -41,7 +41,7 @@ class InstallWorker @AssistedInject constructor(
 
     companion object {
         const val PACKAGE_NAME_KEY = "package_name"
-        const val FAKE_DOWNLOADING_DELAY = 3000L
+        const val FAKE_DOWNLOADING_DELAY = 0L
     }
 
     override suspend fun doWork(): Result = coroutineScope {
