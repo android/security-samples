08a3fa7894a9263ac93cc41966f1f8aab353b1cd
Yacine Rezgui <yrezgui@google.com>
2021-11-22 11:59:05 +0000

    Set ActionLog id to be generated automatically

--- AppInstaller/app/src/main/java/com/samples/appinstaller/database/ActionLog.kt
+++ AppInstaller/app/src/main/java/com/samples/appinstaller/database/ActionLog.kt
@@ -7,7 +7,7 @@ import com.samples.appinstaller.store.PackageName
 
 @Entity(tableName = "action_logs")
 data class ActionLog(
-    @PrimaryKey val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "session_id") val sessionId: Int,
     @ColumnInfo(name = "package_name") val packageName: PackageName,
     @ColumnInfo(name = "type") val type: ActionType,
