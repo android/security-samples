e6c971c4612f41dde4d0e108d2123e7e3241527d
Yacine Rezgui <yrezgui@google.com>
2021-11-22 17:49:43 +0000

    Add coloration on logs

--- AppInstaller/app/src/main/java/com/samples/appinstaller/installer/LogsScreen.kt
+++ AppInstaller/app/src/main/java/com/samples/appinstaller/installer/LogsScreen.kt
@@ -1,28 +1,44 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.samples.appinstaller.installer
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.ListItem
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import com.samples.appinstaller.AppViewModel
 import com.samples.appinstaller.R
 import com.samples.appinstaller.Route
-import com.samples.appinstaller.database.ActionLog
-import com.samples.appinstaller.database.PackageAction
+import com.samples.appinstaller.database.ActionStatus
 
 @Composable
 fun LogsScreen(navController: NavController, viewModel: AppViewModel) {
@@ -32,17 +48,12 @@ fun LogsScreen(navController: NavController, viewModel: AppViewModel) {
         }
     }
 
-    val logs = emptyList<ActionLog>()
+    val logs by viewModel.logs.collectAsState(initial = emptyList())
 
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text(stringResource(R.string.app_name)) },
-                actions = {
-                    IconButton(onClick = { viewModel.refreshLibrary() }) {
-                        Icon(Icons.Filled.Refresh, stringResource(R.string.refresh_library))
-                    }
-                }
             )
         },
         bottomBar = {
@@ -50,14 +61,14 @@ fun LogsScreen(navController: NavController, viewModel: AppViewModel) {
                 BottomNavigationItem(
                     icon = { Icon(Route.Store.icon, contentDescription = null) },
                     label = { Text(Route.Store.title) },
-                    selected = true,
+                    selected = false,
                     onClick = {
                         navController.navigate(Route.Store.id)
                     }
                 )
                 BottomNavigationItem(
                     icon = { Icon(Route.Logs.icon, contentDescription = null) },
-                    label = { Text(Route.Settings.title) },
+                    label = { Text(Route.Logs.title) },
                     selected = true,
                     onClick = {}
                 )
@@ -74,10 +85,21 @@ fun LogsScreen(navController: NavController, viewModel: AppViewModel) {
         content = { innerPadding ->
             LazyColumn(Modifier.padding(innerPadding)) {
                 items(logs) { log ->
+                    val backgroundColor = when (log.status) {
+                        ActionStatus.INITIALIZED -> Color.Transparent
+                        ActionStatus.COMMITTED -> Color.LightGray
+                        ActionStatus.PENDING_USER_ACTION -> Color.Cyan
+                        ActionStatus.SUCCESS -> Color.Green
+                        ActionStatus.FAILURE -> Color.Yellow
+                        ActionStatus.UNKNOWN -> Color.Magenta
+                    }
+
                     ListItem(
+                        modifier = Modifier.background(backgroundColor.copy(alpha = 0.5f)),
                         text = { Text(log.packageName) },
-                        secondaryText = { Text(log.toString()) }
+                        secondaryText = { Text(log.toString()) },
                     )
+                    Divider()
                 }
             }
         }
