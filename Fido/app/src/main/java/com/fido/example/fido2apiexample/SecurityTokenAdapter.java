3ecf2e22687400cd7943805d86f986a4303730c9
wkouki <wkouki@google.com>
2022-10-03 15:29:54 +0900

    - Fix build error for Fido Project.

--- Fido/app/src/main/java/com/fido/example/fido2apiexample/SecurityTokenAdapter.java
+++ Fido/app/src/main/java/com/fido/example/fido2apiexample/SecurityTokenAdapter.java
@@ -16,8 +16,8 @@
 
 package com.fido.example.fido2apiexample;
 
-import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
