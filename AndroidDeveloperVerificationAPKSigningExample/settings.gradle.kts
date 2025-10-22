55d7dcfb877bec7046d8090f3c236b09774ccc15
Todd Burner <todd.burner@gmail.com>
2025-10-22 10:46:08 -0400

    Change folder name (#115)
    * Renamed folder for clarity
    
    * update readme to correct link
    
    * fix workflow
    
    * fix workflow again
    
    * fix workflow again (2nd)
    
    * fix gradle
    
    ---------
    
    Co-authored-by: Todd Burner <tburner@google.com>

--- AndroidDeveloperVerificationAPKSigningExample/settings.gradle.kts
+++ AndroidDeveloperVerificationAPKSigningExample/settings.gradle.kts
@@ -19,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "AndroidDeveloperVerificationOnboarding"
+rootProject.name = "AndroidDeveloperVerificationAPKSigningExample"
 include(":app")
