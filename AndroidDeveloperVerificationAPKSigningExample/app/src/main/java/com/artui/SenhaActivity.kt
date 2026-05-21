d754ba60a67247983b539692902252d91ac41438
Mulher amparada <mulheramparada@gmail.com>
2026-05-21 15:18:19 -0300

    Criar SenhaActivity.kt

---
+++ AndroidDeveloperVerificationAPKSigningExample/app/src/main/java/com/artui/SenhaActivity.kt
@@ -0,0 +1,12 @@
+package com.artui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SenhaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_senha)
+    }
+}
